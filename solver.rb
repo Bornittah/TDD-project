b64960a7a8c645dca25a6d68fb238fc1cedcf7ae
bornittah <bornittah@gmail.com>
2023-02-23 15:32:05 +0300

    Create Solver Class

---
+++ solver.rb
@@ -0,0 +1,29 @@
+class Solver
+  def factorial(num)
+    raise ArgumentError, "Number must be non-negative" if n < 0
+    
+    result = 1
+    (1..num).each do |i|
+      result *= i
+    end
+    
+    result
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+  
+
+  def fizzbuzz(num)
+    if num % 3 == 0 && num % 5 == 0
+      "fizzbuzz"
+    elsif num % 3 == 0
+      "fizz"
+    elsif num % 5 == 0
+      "buzz"
+    else
+      num.to_s
+    end
+  end
+  
